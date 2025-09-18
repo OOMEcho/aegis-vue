617c6bed4462baa0976a561a5172279d6f14a89e
xuesong.lei <228389787@qq.com>
2025-09-18 17:11:12 +0800

    feat: 增加环境变量

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8080',
+        target: process.env.VUE_APP_BASE_API,
         pathRewrite: {
           '^/api': '',
         }
